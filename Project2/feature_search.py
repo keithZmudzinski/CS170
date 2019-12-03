fdeea2e85b0bb9346a3c9b79b110f59e5bbe7d4f
keithZmudzinski <kzmudz79@gmail.com>
2019-12-03 00:07:01 -0800

    Added desired outputs

--- Project2/feature_search.py
+++ Project2/feature_search.py
@@ -1,57 +1,31 @@
+from __future__ import print_function
 import pandas as pd
-import random, math, copy, numpy
-import sys
+import math, sys
+
+output_string = ''
+output_file = open('final_output_small_28.txt', 'w+')
 
 def get_data():
-	data = pd.read_csv('CS170_SMALLtestdata__1.txt', delim_whitespace = True, skipinitialspace=True, header=None)
-	# saved_classes = data.iloc[:, 0].copy()
-	# print('Initial untouched  data:\n', data)
-	# means = data.apply(numpy.mean)
-	# # print('Data after numpy.mean applied:\n', means)
-	# std_deviations = data.apply(numpy.std)
-	# # print('Data after numpy.std applied:\n', std_deviations)
-	# data = data.apply(lambda x : (x - means) / std_deviations)
-	# sys.exit(1)
+	data = pd.read_csv('CS170_SMALLtestdata__28.txt', delim_whitespace = True, skipinitialspace=True, header=None)
+	print('This dataset has %d features (not including the class attribute), with %d instances.\n' % (data.shape[1] - 1, len(data.index)), file=output_file)
+	print('Please wait while I normalize the data...', end = '', file = output_file)
 	data = (data - data.mean()) / data.std()
-	# data.iloc[:, 0] = saved_classes
-	print(data)
+	print(' Done!\n', file = output_file)
 	return data
 
-def calc_distance(row, features, test_row):
-	sum = 0
-	for feature in features:
-		sum += (row.iloc[feature] - test_row.iloc[feature]) ** 2
-	return sum
-
 def one_nearest_neighbor(data, features, test_row_index):
-	# distances = data.apply(calc_distance, axis = 1, args = [features, data.iloc[test_row_index]])
-	# distances =  distances.drop(test_row_index)
-	# smallest_distance = sys.float_info.max
-	# index = -1
-	# for i, distance in enumerate(distances):
-	# 	if distance < smallest_distance:
-	# 		smallest_distance = distance
-	# 		index = i
-	# # sys.exit(1)
-	# return data.iat[index, 0]
-    smallest_distance = sys.float_info.max
-    guessed_class_index = -1
-    for i in range(len(data.index)):
-        sum = 0
-        for feature in features:
-            temp1 = data.iat[i, feature]
-            temp2 = data.iat[test_row_index, feature]
-            # print(temp2)
-            sum += (temp1 - temp2) ** 2
-        curr_dist = sum
-        # print(curr_dist)
-        if curr_dist < smallest_distance and i != test_row_index:
-            smallest_distance = curr_dist
-            guessed_class_index = i
-    # print(smallest_distance, curr_dist)
-    return data.iat[guessed_class_index, 0]
-
-
+	smallest_distance = sys.float_info.max
+	smallest_distance_loc = -1
+	for i in range(len(data.index)):
+		if i == test_row_index:
+			continue
+		local_distance = 0
+		for feature in features:
+			local_distance += (data.iat[test_row_index, feature] - data.iat[i, feature]) ** 2
+		if local_distance < smallest_distance:
+			smallest_distance = local_distance
+			smallest_distance_loc = i
+	return data.iat[smallest_distance_loc, 0]
 
 def leave_1_out_cross_validation(data, current_set, feature_to_add, best_so_far_accuracy):
 	MAX_ALLOWED_INCORRECT = len(data.index) * (1 - best_so_far_accuracy)
@@ -59,46 +33,57 @@ def leave_1_out_cross_validation(data, current_set, feature_to_add, best_so_far_
 
 	for i in range(len(data.index)):
 		if incorrect_so_far > MAX_ALLOWED_INCORRECT:
-			# print('speed up worked, %d incorrect' % incorrect_so_far)
 			return -1
 		correct_class = data.iat[i, 0]
 		predicted_class = one_nearest_neighbor(data, current_set + [feature_to_add], i)
 		# Because comparing floats
-		if math.fabs(correct_class - predicted_class) > .001:
+		if math.fabs(correct_class - predicted_class) > .01:
 			incorrect_so_far += 1
 	return (len(data.index) - incorrect_so_far) / float(len(data.index))
 
 
 def feature_search(data):
+	global output_string
+	output_string += 'Beginning search.\n\n'
 	current_set_of_features = []
 	sets_of_features = []
 	accuracies = []
+	absolute_best_accuracy = 0
 
 	for i in range(1, data.shape[1]):
-		print('On the %dth level of the search tree' % i)
 		feature_to_add_at_this_level = -1
-		best_so_far_accuracy = 0;
 		accuracy_was_improved = False
+		best_so_far_accuracy = 0;
 
 		for k in range(1, data.shape[1]):
 			if k not in current_set_of_features:
-				print('--Considering adding the %dth feature' % k)
 				accuracy = leave_1_out_cross_validation(data, current_set_of_features, k, best_so_far_accuracy)
-				# print('accuracy with %dth feature: %f' % (k, accuracy))
+				output_features = current_set_of_features[:]
+				output_features.append(k)
+				output_string += '		Using feature(s) ' + str(output_features) + ' accuracy is %.1f%%\n' % (round(accuracy, 3) * 100)
 				if accuracy > best_so_far_accuracy:
-					print('Accuracy improved from %f to %f' % (best_so_far_accuracy, accuracy))
 					best_so_far_accuracy = accuracy
 					feature_to_add_at_this_level = k
-		accuracies.append(copy.copy(best_so_far_accuracy))
-		current_set_of_features.append(feature_to_add_at_this_level)
-		sets_of_features.append(current_set_of_features[:])
-		print('On level %d I added feature %d to current set; current features are %s\n' % (i, feature_to_add_at_this_level, str(current_set_of_features)))
+				if accuracy > absolute_best_accuracy:
+					accuracy_was_improved = True
 
+		accuracies.append(best_so_far_accuracy)
+		current_set_of_features.append(feature_to_add_at_this_level)
+		absolute_best_accuracy = best_so_far_accuracy if (best_so_far_accuracy > absolute_best_accuracy) else absolute_best_accuracy
+		output_string = output_string + '\n\n(Warning, Accuracy has decreased! Continuing search in case of local maxima)' if not(accuracy_was_improved) else output_string
+		output_string += '\n\nFeature set ' + str(current_set_of_features) + ' was best, accuracy is %.1f%%\n\n' % (round(best_so_far_accuracy, 3) * 100)
+		sets_of_features.append(current_set_of_features[:])
 	return sets_of_features, accuracies
 
-if __name__ == '__main__':
-	data = get_data()
-	# print(data)
-	features, accuracies = feature_search(data)
-	for feature, accuracy in zip(features, accuracies):
-		print(feature, accuracy)
+
+data = get_data()
+features, accuracies = feature_search(data)
+best_accuracy = max(accuracies)
+best_features = features[accuracies.index(best_accuracy)]
+best_features = '{' + ', '.join([str(feature) for feature in best_features]) + '}'
+print('Running nearest neighbor with all %d features, using "leave-one-out" evaluation, I get an accuracy of %.1f%%\n' % (data.shape[1] - 1, round(best_accuracy, 3) * 100), file = output_file)
+print(output_string, file = output_file)
+print('Finished search!! The best feature subset is %s, which has an accuracy of %.1f%%' % (best_features, round(best_accuracy, 3) * 100), file = output_file)
+for feature, accuracy in zip(features, accuracies):
+	print('{:>34}, {}'.format(feature, accuracy), file=output_file)
+output_file.close()
